3f939bba8a0cf7aa897c23ac088d892441c98c89
Lucas Zerino <101360532+LucasZerino@users.noreply.github.com>
2022-10-19 12:11:20 -0300

    att
    att

--- src/server.js
+++ src/server.js
@@ -1,10 +1,13 @@
 const express = require("express")
 const user = require("./routes/user")
+const {PrismaClient} = require("@prisma/client")
+const { response } = require("express")
 var cors = require('cors')
 
 const app = express()
 
 const port = process.env.PORT || 3333
+const prisma = new PrismaClient()
 
 app.use(express.json())
 app.use(user)
@@ -21,4 +24,28 @@ app.get("/registro", (req, res) => {
 
 app.listen(port, () => {
     console.log(`Aplicação rodando na porta: ${port}`)
+})
+
+// Criar usuário
+app.post("/newuser", async (req, res) => {
+    const { user, email, senha} = req.body
+    const createUser = await prisma.users.create({data: {
+        user,
+        email,
+        senha,
+    },
+});
+    return res.status(201).json( {
+        "status": "sucess",
+        "Sucess": "Usuário criado com sucesso!"
+    })
+})
+
+// Ler usuários
+app.get("/listuser", async (req, res) => {
+    const allUsers = await prisma.users.findMany()
+    return res.status(200).json( {
+        "status": "sucess",
+        "Sucess": allUsers
+    })
 })
