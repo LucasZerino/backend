c9c5d3fa017bfcbc002719c82db9ee992c497e7a
Lucas Zerino <101360532+LucasZerino@users.noreply.github.com>
2022-10-31 17:08:14 -0300

    att
    att

--- readme.md
+++
@@ -1,3 +0,0 @@
-// Trocar:
-
-docker-compose.yml - Mudar para parametros do seu Banco 
--- src/server.js
+++ src/server.js
@@ -1,48 +1,10 @@
-const express = require("express")
-var cors = require('cors')
-const {PrismaClient} = require("@prisma/client")
-const { response } = require("express")
+import express from "express";
 
+import { router } from "./routes";
 
-const app = express()
-app.use(cors())
+const app = express();
 
-const port = process.env.PORT || 3333
-const prisma = new PrismaClient()
+app.use(express.json());
+app.use(router);
 
-app.get("/", function (req, res){
-    res.send("Api para Bando de Talentos!")
-})
-
-app.get("/registro", (req, res) => {
-    return res.json("Usuário registrado!")
-})
-
-
-app.listen(port, () => {
-    console.log(`Aplicação rodando na porta: ${port}`)
-})
-
-// Criar usuário
-app.post("/newuser", async (req, res) => {
-    const { user, email, senha} = req.body
-    const createUser = await prisma.users.create({data: {
-        user,
-        email,
-        senha,
-    },
-});
-    return res.status(201).json( {
-        "status": "sucess",
-        "Sucess": "Usuário criado com sucesso!"
-    })
-})
-
-// Ler usuários
-app.get("/listuser", async (req, res) => {
-    const allUsers = await prisma.users.findMany()
-    return res.status(200).json( {
-        "status": "sucess",
-        "Sucess": allUsers
-    })
-})
+app.listen(3030, () => console.log("Server listening on port 3030"));
